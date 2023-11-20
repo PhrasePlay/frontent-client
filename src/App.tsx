b1bbfdcad190b92cbe931b2f23f4f8a573c2bbeb
John Luke <joaolucas.deoliveira56@gmail.com>
2023-11-20 13:27:27 -0300

    feat: add react-router-dom and create login page

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,9 @@
+import { RouterProvider } from 'react-router-dom';
+
+import { router } from './routes';
+
 function App() {
-  return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
+  return <RouterProvider router={router} />;
 }
 
 export default App;
